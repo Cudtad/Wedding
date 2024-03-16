7368f5729bb86e62943b24328c511448cdbe7df2
DatND <datnd@nami.trade>
2024-03-16 23:47:00 +0700

    update

--- src/components/homepage/index.tsx
+++ src/components/homepage/index.tsx
@@ -11,6 +11,15 @@ var duration = require("dayjs/plugin/duration");
 
 dayjs.extend(duration);
 
+const sweetsMemoryList = [
+  "/assets/images/sweet-memory/sweet_memory1.jpeg",
+  "/assets/images/sweet-memory/sweet_memory2.jpeg",
+  "/assets/images/sweet-memory/sweet_memory3.jpeg",
+  "/assets/images/sweet-memory/sweet_memory4.jpeg",
+  "/assets/images/sweet-memory/sweet_memory5.jpeg",
+  "/assets/images/sweet-memory/sweet_memory6.jpeg",
+];
+
 /* eslint-disable @next/next/no-img-element */
 export default function HomePage() {
   const [slideIndex, setSlideIndex] = useState<number>(0);
@@ -159,6 +168,7 @@ export default function HomePage() {
           </div>
         </div>
       </section>
+
       <section className="pt-20 max-w-screen-xl mx-auto">
         <div className="flex flex-col items-center gap-y-4">
           <Image
@@ -173,26 +183,120 @@ export default function HomePage() {
             gánh vác.
           </p>
         </div>
-        <div className="grid grid-cols-11 gap-x-32 mt-12 shadow-2xl py-16 relative">
-          <div className="text-end text-4xl font-allura font-semibold mt-40 col-span-5">
-            Lần đầu chúng ta gặp nhau
-          </div>
-          <div className="col-span-1 flex flex-col items-center justify-between">
-            <div className="h-6 w-6 rounded-full bg-[#c89d9c] -m-8"></div>
-            <div className="h-6 w-6 rounded-full bg-[#c89d9c] -m-8 relative">
-              <div className="h-4 w-4 rounded-full bg-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10"></div>
+        <div className="gap-x-32 mt-12 shadow-2xl py-16 relative">
+          <div className="grid grid-cols-11 ">
+            <div className="text-end text-4xl font-allura font-semibold mt-40 col-span-5">
+              Lần đầu chúng ta gặp nhau
+            </div>
+            <div className="col-span-1 flex flex-col items-center justify-between">
+              <div className="h-6 w-6 rounded-full bg-[#c89d9c] -m-8"></div>
+              <div className="h-6 w-6 rounded-full bg-[#c89d9c] -m-8 relative">
+                <div className=" border border-dashed border-[#c89d9c] w-28 h-0.5 mt-3 absolute right-11"></div>
+                <div className="h-4 w-4 rounded-full bg-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10"></div>
+              </div>
+            </div>
+            <div className="col-span-5 h-[450px]">
+              <Image
+                width={450}
+                height={450}
+                src="/assets/images/couple/couple_husband.jpeg"
+                alt="Story image"
+                className="object-cover object-top h-full"
+              />
             </div>
           </div>
-          <div className="col-span-5 h-[450px]">
-            <Image
-              width={450}
-              height={450}
-              src="/assets/images/couple/couple_husband.jpeg"
-              alt="Story image"
-              className="object-cover object-top h-full"
-            />
+
+          <div className="grid grid-cols-11 mt-10">
+            <div className="col-span-5 h-[450px] flex justify-end">
+              <Image
+                width={450}
+                height={450}
+                src="/assets/images/couple/couple_husband.jpeg"
+                alt="Story image"
+                className="object-cover object-top h-full"
+              />
+            </div>
+            <div className="col-span-1 flex flex-col items-center justify-end">
+              <div className="h-6 w-6 rounded-full bg-[#c89d9c] -m-8 relative">
+                <div className=" border border-dashed border-[#c89d9c] w-28 h-0.5 mt-3 ml-11 "></div>
+                <div className="h-4 w-4 rounded-full bg-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10"></div>
+              </div>
+            </div>
+            <div className="text-start text-4xl font-allura font-semibold mt-40 col-span-5">
+              Ngày cô ấy đồng ý lời tỏ tình
+            </div>
           </div>
+
+          <div className="grid grid-cols-11 mt-10">
+            <div className="text-end text-4xl font-allura font-semibold mt-40 col-span-5">
+              Ngày cô ấy đồng ý lời cầu hôn
+            </div>
+
+            <div className="col-span-1 flex flex-col items-center justify-end">
+              <div className="h-6 w-6 rounded-full bg-[#c89d9c] -m-8 relative">
+                <div className=" border border-dashed border-[#c89d9c] w-28 h-0.5 mt-3 absolute right-11 "></div>
+                <div className="h-4 w-4 rounded-full bg-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10"></div>
+              </div>
+            </div>
+            <div className="col-span-5 h-[450px] flex justify-start">
+              <Image
+                width={450}
+                height={450}
+                src="/assets/images/couple/couple_husband.jpeg"
+                alt="Story image"
+                className="object-cover object-top h-full"
+              />
+            </div>
+          </div>
+
+          <div className="grid grid-cols-11 mt-10">
+            <div className="col-span-5 h-[450px] flex justify-end">
+              <Image
+                width={450}
+                height={450}
+                src="/assets/images/couple/couple_husband.jpeg"
+                alt="Story image"
+                className="object-cover object-top h-full"
+              />
+            </div>
+            <div className="col-span-1 flex flex-col items-center justify-end">
+              <div className="h-6 w-6 rounded-full bg-[#c89d9c] -m-8 relative"></div>
+            </div>
+            <div className="text-start text-4xl font-allura font-semibold mt-40 col-span-5">
+              Ngày chúng ta bên nhau trọn đời
+            </div>
+          </div>
+
           <div className="w-1 absolute top-8 bottom-8 left-1/2 -translate-x-1/2 bg-[#c89d9c]"></div>
+        </div>
+      </section>
+
+      <section className="mt-40 max-w-screen-xl mx-auto">
+        <div className="flex flex-col items-center gap-y-4">
+          <Image
+            width={126}
+            height={60}
+            src="/assets/images/couple/sec-title-flower.png"
+            alt="flower"
+          />
+          <h3 className=" font-allura text-5xl text-black">Sweets memories</h3>
+          <p className=" text-base text-[#444] font-poppins">
+            Yêu nhau là sống bên nhau đến trọn đời là cùng chung vui và cùng
+            gánh vác.
+          </p>
+        </div>
+        <div className="grid grid-cols-3 gap-10">
+          {sweetsMemoryList.map((item) => {
+            return (
+              <div key={item} className="w-full h-full overflow-hidden">
+                <img
+                  src={item}
+                  alt="Image sweet memories"
+                  className="hover:scale-110 transition-all"
+                />
+              </div>
+            );
+          })}
         </div>
       </section>
     </>
